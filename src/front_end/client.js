81c27c4e6bd98953cda454a2421d248dd56b640a
Tim Creasy <timcreasy@me.com>
2016-09-12 10:56:04 -0500

    Added react-router for routing

--- src/front_end/client.js
+++ src/front_end/client.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {Router, Route, hashHistory} from 'react-router';
 import Application from "./components/Application";
+import Posts from "./components/PostContainer";
 import Register from "./components/Register";
 import Login from "./components/Login";
 
@@ -12,9 +13,10 @@ import Login from "./components/Login";
 
 ReactDOM.render((
   <Router history={hashHistory}>
-    <Route path="/" component={Application}>
+    <Route path="/" component={Application} >
+      <Route path="/posts" component={Posts} />
       <Route path="/login" component={Login}/>
-      <Route path="/register" component={register}/>
+      <Route path="/register" component={Register}/>
     </Route>
   </Router>
 ), document.getElementById('app'));
--- src/front_end/components/Application.js
+++ src/front_end/components/Application.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Link } from "react-router";
 import PostContainer from "./PostContainer";
+import Login from "./Login";
+import Register from "./Register";
 
 let Application = React.createClass({
   render: function() {
     return (
-      <div>
-        <PostContainer />
+      <div className='container'>
+        <ul className='nav navbar-nav' role='nav'>
+          <li><Link to="/posts">Posts</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/register">Register</Link></li>
+        </ul>
+        {this.props.children}
       </div>
     );
   }
