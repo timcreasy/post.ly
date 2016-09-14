3de1620cbfc970422bd7bf0b161924837b9d2c5c
Tim Creasy <timcreasy@me.com>
2016-09-14 09:04:20 -0500

    Added event system for login

--- src/front_end/components/Application.js
+++ src/front_end/components/Application.js
@@ -4,16 +4,28 @@ import PostContainer from "./PostContainer";
 import Login from "./Login";
 import Register from "./Register";
 import { IndexLink } from 'react-router';
+import emitter from "../../events/AppEvents";
 
 let Application = React.createClass({
+
+
+
   render: function() {
+    emitter.addListener('loginEvent', (username) => {
+      console.log("SHIT SOMEONE LOGGED IN:", username);
+    });
+
     return (
       <div className='container'>
-        <ul className='nav navbar-nav' role='nav'>
-          <li><IndexLink to="/" activeClassName="active">Posts</IndexLink></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
-        </ul>
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <ul className='nav navbar-nav' role='nav'>
+              <li><IndexLink to="/" activeClassName="active">Posts</IndexLink></li>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/register">Register</Link></li>
+            </ul>
+          </div>
+        </nav>
         {this.props.children}
       </div>
     );
--- src/front_end/components/Login.js
+++ src/front_end/components/Login.js
@@ -1,7 +1,12 @@
 import React from "react";
 import LoginForm from "./Login/LoginForm";
+import emitter from "../../events/AppEvents";
 
 let Login = React.createClass({
+
+  getInitialState() {
+    return ({ currentUser: "" });
+  },
 
   getUser(token) {
     $.ajax({
@@ -9,7 +14,7 @@ let Login = React.createClass({
       method: 'GET',
       headers: {'X-Auth': token },
       success: (data) => {
-        console.log("SUCCESS LOGIN");
+        emitter.emit('loginEvent', this.state.currentUser);
       },
       error: (xhr, textStatus, err) => {
         console.log("Oops, something happened");
@@ -26,6 +31,7 @@ let Login = React.createClass({
       contentType: "application/json",
       data: JSON.stringify(user),
       success: (data) => {
+        this.setState({ currentUser: user.username });
         this.getUser(data);
       },
       error: (xhr, textStatus, err) => {
@@ -35,6 +41,7 @@ let Login = React.createClass({
   },
 
   render: function() {
+
     return (
       <div>
         <h1>Login</h1>
